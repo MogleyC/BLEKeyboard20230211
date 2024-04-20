9b1edd54400873e042407743a174627cee44a7d6
KSG <indra1469@naver.com>
2024-04-20 20:28:47 +0900

    KSG : 테스트 코드 수정

--- VSCode/BLEKeyboard/src/main.c
+++ VSCode/BLEKeyboard/src/main.c
@@ -1,43 +1,49 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>c
+#include <zephyr/drivers/gpio.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(my_module, LOG_LEVEL_INF);
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 1000
+#define SLEEP_TIME_MS 500
 
 // /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+// how logging
+// LOG_INF("Hi!!");
+// printk("Hey! %d\n", cnt);
+
 int main(void)
 {
-        int ret;
+	int ret;
 
-        if (!gpio_is_ready_dt(&led))
-        {
-                return 0;
-        }
+	if (!gpio_is_ready_dt(&led))
+	{
+		return 0;
+	}
 
-        ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-        if (ret < 0)
-        {
-                return 0;
-        }
+	gpio_flags_t flags = GPIO_OUTPUT_INACTIVE;	//GPIO_OUTPUT
+	ret = gpio_pin_configure_dt(&led, flags);
+	// gpio_pin_set_dt(&led, 0);
+	if (ret < 0)
+	{
+		return 0;
+	}
 
-        while (1)
-        {
-                ret = gpio_pin_toggle_dt(&led);
-                // printf("Hello!!\n");
-                LOG_INF("Hello!!");
+	int cnt = 0;
+	while (1)
+	{
+		ret = gpio_pin_toggle_dt(&led);
+		printk("Hey! %d\n", cnt);
+		++cnt;
 
-                if (ret < 0)
-                {
-                        return 0;
-                }
-                k_msleep(SLEEP_TIME_MS);
-        }
-        return 0;
+		if (ret < 0)
+		{
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
 }
